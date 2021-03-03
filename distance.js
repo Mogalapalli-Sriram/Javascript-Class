4407ab3a8d04132f63e7e7f2ebe82d9b99bec0b3
SRIRAM238 <75298072+SRIRAM238@users.noreply.github.com>
2021-03-03 18:01:05 +0530

    updated js file

--- distance.js
+++ distance.js
@@ -10,7 +10,7 @@ class CoordinatePoint {
  }
 }
 
-var point1 = new CoordinatePoint(2,3);
-var point2 = new CoordinatePoint(5,7);
-var distance = CoordinatePoint.Distance(point1,point2);
+let point1 = new CoordinatePoint(2,3);
+let point2 = new CoordinatePoint(5,7);
+let distance = CoordinatePoint.Distance(point1,point2);
 console.log("distance between two coordinate points is:" + distance);
